82619ed29ab6d7b1457eac365b7a6ba35b6eb3b9
Michael L Heuer <heuermh@acm.org>
2024-09-02 09:31:04 -0500

    fix up license header

--- firmware/midi_arp_firmware/midi_arp_firmware.ino
+++ firmware/midi_arp_firmware/midi_arp_firmware.ino
@@ -1,22 +1,23 @@
 /*
 
-    arpeggiator
+    mplsartindustry/arpeggiator
     Copyright (c) 2024 held jointly by the individual authors.
 
-    This file is part of arpeggiator.
+    This file is part of mplsartindustry/arpeggiator.
 
-    arpeggiator is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+    mplsartindustry/arpeggiator is free software: you can redistribute
+    it and/or modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
 
-    arpeggiator is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    mplsartindustry/arpeggiator is distributed in the hope that it will
+    be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+    of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with arpeggiator.  If not, see <http://www.gnu.org/licenses/>.
+    along with mplsartindustry/arpeggiator.  If not, please see
+    <http://www.gnu.org/licenses/>.
 
 */
 
